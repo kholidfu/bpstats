a818cfa5bff17fab5ae7c91cdf5ea6408c3112f6
sopier <sopier@gmail.com>
2013-12-05 12:55:46 +0700

    finisih til pandas

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from unidecode import unidecode
 import urllib2
 import re
 from bs4 import BeautifulSoup
+import pandas as pd
 
 class bpsData(object):
 
@@ -16,7 +18,30 @@ class bpsData(object):
         provinces = [i.text for i in soup.find_all('td', style=re.compile(r"height:15.0pt"))[3:37]]
         return provinces
 
+    def create_csv(self):
+        soup = BeautifulSoup(self.get_html())
+        rows = soup.find_all('tr', style=re.compile("userset;height:15.0pt"))
+        data = ''
+        for row in rows:
+            #r = row.find_all('td')
+            data += ';'.join([r.text for r in row.find_all('td') if r.text != None])
+            data += '\n'
+        data_file = data.split('\n')
+        with open("data.csv", "wb") as f:
+            for d in data_file[6:40]:
+                f.write(d + '\n')
+
+    def analyze_data(self):
+        """analyze data with pandas
+        """
+        df = pd.read_csv("data.csv", sep=";", index_col=0, header=None)
+        data_lk = df[[1,2,3,4]]
+        data_prmp = df[[5,6,7,8]]
+        data_total = df[[9,10,11,12]]
+        print data_total
+
 
 if __name__ == "__main__":
     b = bpsData()
-    print b.get_provinces()
+    b.create_csv()
+    b.analyze_data()
